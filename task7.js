9a2b73ac5288715c92228567f3ac539b20ec4ead
Shaik Amir Ahmed <97598398+BoyBrillant@users.noreply.github.com>
2022-09-05 02:56:40 +0530

    Add files via upload

---
+++ task7.js
@@ -0,0 +1,15 @@
+var myAge=24;
+//my myAge
+var earlyYears=2;
+//Early earlyYears
+earlyYears*=10.5;
+//The calculation of early 2 years of dog
+let laterYears=myAge-2;
+//The dog Late Late years;
+laterYears*=4;
+//dog years accounted by late years;
+const myAgeInDogYears= earlyYears+laterYears;
+//calculation of my age in dog years;
+const myName="Amir".toLowerCase();
+// My name and that too in lowercase.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog year.`);
